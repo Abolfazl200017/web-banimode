c700cc6634f46a17ea0ee6296882e3dddb44822c
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2022-12-29 01:41:16 +0330

    config event layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavbarComponent } from './header/navbar/navbar.component';
+import { EventComponent } from './event/event.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
