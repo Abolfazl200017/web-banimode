c870f10b16fb554fe497f19158173ed993c69043
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2022-12-29 17:07:50 +0330

    config swipper slide show with static data

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import SwiperCore, { FreeMode, Pagination, SwiperOptions } from 'swiper';
-import {SwiperComponent} from 'swiper/angular'
+import SwiperCore, { Autoplay, FreeMode, Navigation, Pagination, Swiper, SwiperOptions } from 'swiper';
 
-SwiperCore.use([Pagination]);
+SwiperCore.use([Pagination, Autoplay , Navigation]);
 @Component({
   selector: 'app-slider',
   templateUrl:'./slider.component.html',
@@ -10,22 +9,8 @@ SwiperCore.use([Pagination]);
 })
 
 export class SliderComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
-  }
-  config: SwiperOptions = {
-    slidesPerView: 3,
-    spaceBetween: 50,
-    navigation: true,
-    pagination: { clickable: true },
-    scrollbar: { draggable: true },
-  };
-  onSwiper([swiper]:any) {
-    console.log(swiper);
-  }
-  onSlideChange() {
-    console.log('slide change');
   }
 }
