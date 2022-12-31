44ffaac8163e99cd88a7f964a024738875fcbdb6
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2022-12-31 23:10:03 +0330

    config event mobile slider

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import SwiperCore, { Autoplay, FreeMode, Navigation, Pagination, Swiper, SwiperOptions,  } from 'swiper';
+
+SwiperCore.use([Pagination, Autoplay , Navigation]);
 
 @Component({
   selector: 'app-event',
@@ -6,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event.component.scss']
 })
 export class EventComponent implements OnInit {
-
+  slides:string[]=[
+    'https://www.banimode.com/img/cms/011010/1672478140.jpg',
+    'https://www.banimode.com/img/cms/011010/1672478164.jpg',
+    'https://www.banimode.com/img/cms/011010/1672478190.jpg',
+    'https://www.banimode.com/img/cms/011010/1672478214.jpg',
+    'https://www.banimode.com/img/cms/011010/1672478198.jpg',
+    'https://www.banimode.com/img/cms/011010/1672478205.jpg',
+  ]
   constructor() { }
 
   ngOnInit(): void {
